c58552e3df6d4f3ea6c71daf7cdd4a4fe64fb06e
tsiontad <tsiontad1216@gmail.com>
2025-09-11 03:35:45 -0400

    fix: set base to /Netflix-Clone-2025/ to resolve 404s on subpath

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/{Netflix-Clone-2025}",
+  base: "/Netflix-Clone-2025/",
   plugins: [react()],
 });
